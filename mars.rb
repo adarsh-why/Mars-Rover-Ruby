366898043094cb9de11dadbb22743f69babf38bd
Adarsh <adarsh.chndrn@gmail.com>
2016-04-12 10:46:46 +0530

    Done with no boundary condition

--- mars.rb
+++ mars.rb
@@ -8,41 +8,41 @@ puts "Enter the moves for Rover"
 moves = gets.chomp.split('')
 
 def dir_update move, curr_dir
-	if curr_dir == 'N'
-		return curr_dir = 'W' if move == 'L'
-		return curr_dir = 'E' if move == 'R'
-	elsif curr_dir == 'E'
-		return curr_dir = 'N' if move == 'L'
-		return curr_dir = 'S' if move == 'R'
-	elsif curr_dir == 'S'
-		return curr_dir = 'E' if move == 'L'
-		return curr_dir = 'W' if move == 'R'
-	elsif curr_dir == 'W'
-		return curr_dir = 'S' if move == 'L'
-		return curr_dir = 'N' if move == 'R'				
-	end
+    if curr_dir == 'N'
+        return curr_dir = 'W' if move == 'L'
+        return curr_dir = 'E' if move == 'R'
+    elsif curr_dir == 'E'
+        return curr_dir = 'N' if move == 'L'
+        return curr_dir = 'S' if move == 'R'
+    elsif curr_dir == 'S'
+        return curr_dir = 'E' if move == 'L'
+        return curr_dir = 'W' if move == 'R'
+    elsif curr_dir == 'W'
+        return curr_dir = 'S' if move == 'L'
+        return curr_dir = 'N' if move == 'R'                
+    end
 end
 
 def move_update move, x, y, curr_dir
-	case curr_dir
-		when 'N'
-		return x, y+1
-		when 'E'
-		return x+1, y
-		when 'W'
-		return x-1, y
-		when 'S'
-		return x, y-1
-	end
+    case curr_dir
+        when 'N'
+        return x, y+1
+        when 'E'
+        return x+1, y
+        when 'W'
+        return x-1, y
+        when 'S'
+        return x, y-1
+    end
 end
 
 moves.each do |move|
-	if move == 'M'
-		x,y = move_update move, x, y, curr_dir
-	elsif move == 'L' or move == 'R' 
-		curr_dir = dir_update move, curr_dir
-	else
-		raise ArgumentError, "Wrong input for Rover movement"
-	end
+    if move == 'M'
+        x,y = move_update move, x, y, curr_dir
+    elsif move == 'L' or move == 'R' 
+        curr_dir = dir_update move, curr_dir
+    else
+        raise ArgumentError, "Wrong input for Rover movement"
+    end
 end
 puts "\n\n#{x}  #{y}  #{curr_dir}"
