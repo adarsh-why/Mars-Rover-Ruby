45f099406a75ef7384eecc5b8b4fd767fa2e28d8
Adarsh <adarsh.chndrn@gmail.com>
2016-04-12 11:34:23 +0530

    handled out of plateau exception

--- mars.rb
+++ mars.rb
@@ -1,9 +1,12 @@
 puts "Co-ordinates of Plateau end"
-plateau_end = gets.split.map!{|num| num.to_i}
+max_x, max_y = gets.split.map!{|num| num.to_i}
+
 puts "Enter starting position of Rover"
 start_pos = gets.split
 curr_dir = start_pos[-1].upcase
+
 x,y = start_pos[0..1].map!{|num| num.to_i}
+
 puts "Enter the moves for Rover"
 moves = gets.chomp.split('')
 
@@ -39,6 +42,7 @@ end
 moves.each do |move|
     if move == 'M'
         x,y = move_update move, x, y, curr_dir
+        raise ArgumentError, "Rover is out of the Plateau" if x > max_x or y > max_y
     elsif move == 'L' or move == 'R' 
         curr_dir = dir_update move, curr_dir
     else
